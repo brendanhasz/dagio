b4fb4bd05118cb64e8220039a127c0dcbdc30450
Brendan Hasz <winsto99@gmail.com>
2021-10-14 18:44:25 +0000

    style

--- src/dagio/run_async_decorator.py
+++ src/dagio/run_async_decorator.py
@@ -4,6 +4,7 @@ from functools import partial
 
 def run_async(fn):
     """Runs a non-async function in a thread pool."""
+
     async def wrapped_fn(*args, **kwargs):
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, partial(fn, *args, **kwargs))
